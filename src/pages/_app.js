8455f4b550af68c6be05d67e2f58900e61c3b817
Sparshlohana <sparsh.lohana@gmail.com>
2023-07-05 12:39:01 +0530

    favicon added

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -4,10 +4,14 @@ import { ToastContainer } from 'react-toastify'
 import 'react-toastify/dist/ReactToastify.css';
 import Footer from '@/components/footer/Footer';
 import "animate.css";
+import Head from 'next/head';
 
 export default function App({ Component, pageProps }) {
   return (
     <>
+      <Head>
+        <link rel="icon" type="image/x-icon" href="/logo.png" />
+      </Head>
       <ToastContainer
         position="top-center"
         autoClose={5000}
